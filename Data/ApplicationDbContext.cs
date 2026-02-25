eb6b05e16af0863ca0cddcd2eecda880eabdb1bc
Lisbeth <yoselynespinoza27@gmail.com>
2026-02-24 22:12:59 -0500

    feat: map entities to uppercase Oracle tables in ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,5 +3,25 @@ namespace LinkVault_Proyecto.Data
 {
     public class ApplicationDbContext : DbContext
 {
-}
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+        // Estos DbSets son los que te permiten consultar las tablas desde C#
+        public DbSet<LinkVault_Proyecto.Models.Categoria> Categorias { get; set; }
+        public DbSet<LinkVault_Proyecto.Models.Recurso> Recursos { get; set; }
+        // Agrega este bloque para mapear correctamente con Oracle
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Forzamos a que el ORM busque las tablas en MAYÚSCULAS
+            modelBuilder.Entity<LinkVault_Proyecto.Models.Categoria>()
+                .ToTable("CATEGORIAS");
+
+            modelBuilder.Entity<LinkVault_Proyecto.Models.Recurso>()
+                .ToTable("RECURSOS");
+        }
+    }
 }
