3f4164c9cd1150d6e028f596fc19ba3662aca407
Lisbeth <yoselynespinoza27@gmail.com>
2026-02-24 22:11:44 -0500

    feat: configure Oracle table mapping in ApplicationDbContext

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
 using LinkVault_Proyecto.Components;
+using Microsoft.EntityFrameworkCore;
+using LinkVault_Proyecto.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 1. Configuración de Servicios
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Inyección del DbContext para Oracle
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseOracle(builder.Configuration.GetConnectionString("OracleConn")));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// 2. Configuración del Pipeline de HTTP
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
